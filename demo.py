24689d0fdd39b8826f727cc879b75e2d89fd4da9
suxiaochen92 <2893948992@qq.com>
2020-05-26 09:55:34 +0800

    顶顶顶顶

---
+++ demo.py
@@ -0,0 +1,45 @@
+# a=input('请输入需要计算的数字：')
+# print('共计：',len(a))
+
+# a=input('请输入需要计算的内容：')
+# # b=a.count(' ')
+# # print('字数合计：',len(a)-b,'字。')
+# b=0
+# for i in a:
+#     if i==' ':
+#         b+=1
+# print(len(a)-b)
+
+# a=(1,2,3,'qwe','你好')
+# b=a.count(1)
+# c=a.index('qwe')
+# print(b,c)
+
+# q=[1,2,55,11,66,33,22,14,21]
+# q.append('append插入')
+# print(q)
+# q.insert(0,'insert插入')
+# print(q)
+# q.extend(['55','11','22'])
+# print(q)
+# w=q.pop(3)
+# print(w)
+# q.reverse()
+# q.sort()
+# q.sort(reverse=True)
+# print(q)
+
+# 冒泡排序
+# q=[1,2,55,11,66,33,22,14,21]
+# for i in range(len(q)-1):
+#     for j in range(len(q)-i-1):
+#         if q[j]>q[j+1]:
+#             q[j],q[j+1]=q[j+1],q[j]
+#     print(q)
+
+# p={
+#     'name':'张三',
+#     'age':17,
+#     'high':'175cm'
+# }
+# print(p['name'])
